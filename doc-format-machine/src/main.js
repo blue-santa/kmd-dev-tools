5e4e29a74370d18430e7f98264d9ec05fbda173c
siddhartha <siddharthakomodo@gmail.com>
2018-12-11 23:15:06 -0900

    add vuetify

--- doc-format-machine/src/main.js
+++ doc-format-machine/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+import 'font-awesome/css/font-awesome.min.css'
 
 new Vue({
   render: h => h(App),
